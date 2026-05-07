adc3113e04df59eba652a962204b2d7e3e41cc97
ca <himthegoat@aol.com>
2026-05-06 21:32:17 -0700

    v2.0: visual tokens + fonts

--- src/components/GrainOverlay.tsx
+++ src/components/GrainOverlay.tsx
@@ -1,8 +1,3 @@
 export default function GrainOverlay() {
-  return (
-    <>
-      <div className="overlay-grain" aria-hidden="true" />
-      <div className="overlay-scan" aria-hidden="true" />
-    </>
-  );
+  return <div className="overlay-grain" aria-hidden="true" />;
 }
